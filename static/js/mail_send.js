6619bff0106e38c6fbb5776e8551355e223e3bad
Kien.Kay <thanhkien76qn@gmail.com>
2022-03-02 22:03:22 +0700

    Kay

--- static/js/mail_send.js
+++ static/js/mail_send.js
@@ -126,7 +126,51 @@ function send_mail_input() {
     var message = jQuery("#message").val();
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     var flag = 0;
+    // if (name == "") {
+    //     jQuery("#name").addClass('invalid');
+    //     jQuery("#val_user_name").html("Your Name is Required");
+    //     flag = 1;
+    // } else {
+    //     jQuery("#name").removeClass('invalid');
+    //     jQuery("#val_user_name").html("");
+    // }
 
+    // if (email == "") {
+    //     jQuery("#email").addClass('invalid');
+    //     jQuery("#val_user_email").html("Please Enter Email");
+    //     flag = 1;
+    // } else if (!email.match(mailformat)) {
+    //     jQuery("#email").addClass('invalid');
+    //     jQuery("#val_user_email").html("Please Enter Valid Email");
+    //     flag = 2;
+    // } else {
+    //     jQuery("#email").removeClass('invalid');
+    //     jQuery("#val_user_email").html("");
+    // }
+
+    // // if (subject == "") {
+    // //     jQuery("#subject").addClass('invalid');
+    // //     jQuery("#val_subject").html("Subject is Required");
+    // //     flag = 1;
+    // // } else {
+    // //     jQuery("#subject").removeClass('invalid');
+    // //     jQuery("#val_subject").html("");
+    // // }
+    // if (message == "") {
+    //     jQuery("#message").addClass('invalid');
+    //     jQuery("#val_message").html("Please Describe your thoughts");
+    //     flag = 1;
+    // } else {
+    //     jQuery("#message").removeClass('invalid');
+    //     jQuery("#val_message").html("");
+    // }
+
+    // if (flag == 1) {
+    //     setTimeout(() => { alert("Please enter full information.") }, 500)
+    // } else {
+    //     if (flag == 2) {
+    //         setTimeout(() => { alert("Please enter correct email format.") }, 500)
+    //     } else {
             Email.send({
                     Host: "smtp.gmail.com",
                     Username: "kay.mailserver@gmail.com",
@@ -139,5 +183,6 @@ function send_mail_input() {
                 // .then(function(message) {
                 //     alert("Message has been sent successfully.")
                 // });
-    
+    //     }
+    // }
 }
