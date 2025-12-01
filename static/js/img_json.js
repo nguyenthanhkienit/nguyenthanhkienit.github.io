1bb87b94593d921476790c6a192d6f237403ce4e
IT: Kay.Kien <kay.kien@feavn.local>
2025-12-01 16:55:11 +0700

    Update

--- static/js/img_json.js
+++ static/js/img_json.js
@@ -1,4 +1,8 @@
   const images = [
         { src: "images/kh/z7278077003569_894794335706cf345d7aed168c5ad64c.jpg", caption: "29-11-2025" },
         { src: "images/kh/.jpg", caption: "..." },
+        { src: "images/kh/.jpg", caption: "..." },
+        { src: "images/kh/.jpg", caption: "..." },
+        { src: "images/kh/.jpg", caption: "..." },
+        { src: "images/kh/.jpg", caption: "..." },
     ];
