4f3194329be3e95e2ddd9cbd9c78570fc8692819
IT: Kay.Kien <kay.kien@feavn.local>
2025-12-01 16:18:29 +0700

    Update

--- static/js/img_json.js
+++ static/js/img_json.js
@@ -1,3 +1,4 @@
   const images = [
-        { src: "images/kh/z6815849117951_88baae4c80be3194552a9bb404748621.jpg", caption: "29-11-2025" },
+        { src: "images/kh/z7278077003569_894794335706cf345d7aed168c5ad64c.jpg", caption: "29-11-2025" },
+        { src: "images/kh/.jpg", caption: "..." },
     ];
