9d5dfa6c47c327fc84daf5c53ca69a4c4bced821
IT: Kay.Kien <kay.kien@feavn.local>
2025-12-29 11:24:09 +0700

    Update img_json.js

--- static/js/img_json.js
+++ static/js/img_json.js
@@ -2,6 +2,6 @@
         { src: "images/kh/z7278077003569_894794335706cf345d7aed168c5ad64c.jpg", caption: "29-11-2025" },
         { src: "images/kh/z7303151556698_7f10847b006b284c29b104c678bc1ed3.jpg", caption: "07-12-2025" },
         { src: "images/kh/z7327108147937_a095f7fd359675c258ad7aeb6b1cca2c.jpg", caption: "14-12-2025" },
-        { src: "images/kh/z7351479350110_6f9647be60445be96f6a31a703fa7b16.jpeg", caption: "21-12-2025" },
-        { src: "images/kh/IMG_4993.jpg", caption: "28-12-2025" },
+        { src: "images/kh/z7351479350110_6f9647be60445be96f6a31a703fa7b16.jpg", caption: "21-12-2025" },
+        { src: "images/kh/IMG_4993.jpeg", caption: "28-12-2025" },
     ];
