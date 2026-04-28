8f3815e0c45335f0d7b9859f85cdff9e62d95706
IT: Kay.Kien <kay.kien@feavn.local>
2026-04-28 15:14:01 +0700

    Update trip

---
+++ static/js/img_trips-data.js
@@ -0,0 +1,104 @@
+const tripsData = {
+        trip1: {
+            title: "Tết (Quảng Ngãi)",
+            images: [
+                "images/kh/29-03-2026.jpg",
+                "images/kh/05-04-2026.jpg",
+                "images/kh/25-03-2026.jpg",
+                "images/kh/25-03-2026 (4).jpg",
+                "images/kh/29-03-2026 (8).jpg",
+                "images/kh/29-03-2026 (9).jpg",
+                "images/kh/14-02-2026 (10).jpg",
+                "images/kh/29-03-2026.jpg",
+                "images/kh/05-04-2026.jpg",
+                "images/kh/25-03-2026.jpg",
+                "images/kh/25-03-2026 (4).jpg",
+                "images/kh/29-03-2026 (8).jpg",
+                "images/kh/29-03-2026 (9).jpg",
+                "images/kh/14-02-2026 (10).jpg",
+                "images/kh/29-03-2026.jpg",
+                "images/kh/05-04-2026.jpg",
+                "images/kh/25-03-2026.jpg",
+          
+                "images/kh/29-03-2026 (9).jpg",
+                "images/kh/14-02-2026 (10).jpg",
+                "images/kh/29-03-2026.jpg",
+                "images/kh/05-04-2026.jpg",
+                "images/kh/25-03-2026.jpg",
+                "images/kh/25-03-2026 (4).jpg",
+                "images/kh/29-03-2026 (8).jpg",
+                "images/kh/29-03-2026 (9).jpg",
+              
+                "images/kh/29-03-2026.jpg",
+                "images/kh/05-04-2026.jpg",
+                "images/kh/25-03-2026.jpg",
+             
+                "images/kh/29-03-2026 (8).jpg",
+                "images/kh/29-03-2026 (9).jpg",
+                "images/kh/14-02-2026 (10).jpg",
+                "images/kh/29-03-2026.jpg",
+                "images/kh/05-04-2026.jpg",
+                "images/kh/25-03-2026.jpg",
+                "images/kh/25-03-2026 (4).jpg",
+                "images/kh/29-03-2026 (8).jpg",
+             
+                "images/kh/14-02-2026 (10).jpg"
+            ]
+        },
+        trip2: {
+            title: "Hội An (Đà Nẵng)",
+            images: [
+                "images/kh/29-03-2026.jpg",
+                "images/kh/05-04-2026.jpg",
+                "images/kh/25-03-2026.jpg",
+                "images/kh/25-03-2026 (4).jpg",
+                "images/kh/29-03-2026 (8).jpg",
+                "images/kh/29-03-2026 (9).jpg",
+                "images/kh/14-02-2026 (10).jpg",
+                "images/kh/29-03-2026.jpg",
+                "images/kh/05-04-2026.jpg",
+                "images/kh/25-03-2026.jpg",
+                "images/kh/25-03-2026 (4).jpg",
+                "images/kh/29-03-2026 (8).jpg",
+                "images/kh/29-03-2026 (9).jpg",
+            
+                "images/kh/29-03-2026.jpg",
+                "images/kh/05-04-2026.jpg",
+                "images/kh/25-03-2026.jpg",
+                "images/kh/25-03-2026 (4).jpg",
+                "images/kh/29-03-2026 (8).jpg",
+                "images/kh/29-03-2026 (9).jpg",
+                "images/kh/14-02-2026 (10).jpg",
+                "images/kh/29-03-2026.jpg",
+                "images/kh/05-04-2026.jpg",
+                "images/kh/25-03-2026.jpg",
+                "images/kh/25-03-2026 (4).jpg",
+                "images/kh/29-03-2026 (8).jpg",
+                "images/kh/29-03-2026 (9).jpg",
+                "images/kh/14-02-2026 (10).jpg",
+                "images/kh/29-03-2026.jpg",
+      
+                "images/kh/25-03-2026.jpg",
+                "images/kh/25-03-2026 (4).jpg",
+                "images/kh/29-03-2026 (8).jpg",
+                "images/kh/29-03-2026 (9).jpg",
+            
+                "images/kh/29-03-2026.jpg",
+                "images/kh/05-04-2026.jpg",
+                "images/kh/25-03-2026.jpg",
+                "images/kh/25-03-2026 (4).jpg",
+                "images/kh/29-03-2026 (8).jpg",
+                "images/kh/29-03-2026 (9).jpg",
+                "images/kh/14-02-2026 (10).jpg"
+            ]
+        },
+        trip3: {
+            title: "Đồi Bát Úp - Thác Liêng Ài (Lâm Đồng)",
+            images: [
+                "images/kh/29-03-2026.jpg",
+                "images/kh/05-04-2026.jpg",
+                "images/kh/25-03-2026.jpg",
+             
+            ]
+        }
+    };
